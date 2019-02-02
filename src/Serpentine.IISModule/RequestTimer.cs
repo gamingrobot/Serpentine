2d335833eed7f272254b0c15413e78624463ef12
Morgan Creekmore <morgan@creekmore.email>
2019-02-02 00:45:13 -0600

    Moved stopwatches to their own class

---
+++ src/Serpentine.IISModule/RequestTimer.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace Serpentine.IISModule
+{
+    internal class RequestTimer
+    {
+        private readonly Stopwatch _requestStopwatch;
+        private readonly Stopwatch _handlerStopwatch;
+
+        public RequestTimer()
+        {
+           _requestStopwatch = new Stopwatch();
+           _handlerStopwatch = new Stopwatch();
+        }
+
+        public void StartRequestTimer()
+        {
+            _requestStopwatch.Start();
+        }
+
+        public void StopRequestTimer()
+        {
+            _requestStopwatch.Stop();
+        }
+
+        public void StartHandlerTimer()
+        {
+            _handlerStopwatch.Start();
+        }
+
+        public void StopHandlerTimer()
+        {
+            _handlerStopwatch.Stop();
+        }
+
+        public long GetRequestMilliseconds()
+        {
+            return _requestStopwatch.ElapsedMilliseconds;
+        }
+
+        public long GetHandlerMilliseconds()
+        {
+            return _handlerStopwatch.ElapsedMilliseconds;
+        }
+
+    }
+}
--- src/Serpentine.IISModule/SerpentineModule.cs
+++ src/Serpentine.IISModule/SerpentineModule.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Net.Mime;
 using System.Web;
 
@@ -7,6 +6,8 @@ namespace Serpentine.IISModule
 {
     public class SerpentineModule : IHttpModule
     {
+        private const string RequestTimerKey = "SerpentineRequestTimer";
+
         public void Init(HttpApplication context)
         {
             context.BeginRequest += OnBeginRequest;
@@ -17,54 +18,50 @@ namespace Serpentine.IISModule
 
         private void OnBeginRequest(object sender, EventArgs e)
         {
-            var app = (HttpApplication)sender;
+            var app = (HttpApplication) sender;
             var context = app.Context;
 
-            var stopwatch = Stopwatch.StartNew();
-            context.Items.Add("SerpentineRequestTimer", stopwatch);
+            var requestTimer = new RequestTimer();
+            context.Items.Add(RequestTimerKey, requestTimer);
+
+            requestTimer.StartRequestTimer();
         }
 
         private void OnPreRequestHandlerExecute(object sender, EventArgs e)
         {
-            var app = (HttpApplication)sender;
+            var app = (HttpApplication) sender;
             var context = app.Context;
+            var requestTimer = (RequestTimer) context.Items[RequestTimerKey];
 
-            var stopwatch = Stopwatch.StartNew();
-            context.Items.Add("SerpentineHandlerTimer", stopwatch);
+            requestTimer.StartHandlerTimer();
         }
 
         private void OnPostRequestHandlerExecute(object sender, EventArgs e)
         {
-            var app = (HttpApplication)sender;
+            var app = (HttpApplication) sender;
             var context = app.Context;
+            var requestTimer = (RequestTimer) context.Items[RequestTimerKey];
 
-            var stopwatch = (Stopwatch)context.Items["SerpentineHandlerTimer"];
-            stopwatch.Stop();
-            var result = stopwatch.ElapsedMilliseconds;
-
-            context.Items.Add("SerpentineHandlerTime", result);
+            requestTimer.StopHandlerTimer();
         }
 
         private void OnEndRequest(object sender, EventArgs e)
         {
-            var app = (HttpApplication)sender;
+            var app = (HttpApplication) sender;
             var context = app.Context;
 
-            var stopwatch = (Stopwatch)context.Items["SerpentineRequestTimer"];
-            stopwatch.Stop();
-            var requestElapsed = stopwatch.ElapsedMilliseconds;
-
-            var handlerElapsed = (long) context.Items["SerpentineHandlerTime"];
+            var requestTimer = (RequestTimer) context.Items[RequestTimerKey];
+            requestTimer.StopRequestTimer();
 
             //TODO get body size, calculate min, max, average
 
-            context.Response.AppendHeader("X-Serpentine-RequestTime", requestElapsed.ToString());
-            context.Response.AppendHeader("X-Serpentine-HandlerTime", handlerElapsed.ToString());
+            context.Response.AppendHeader("X-Serpentine-RequestTime", requestTimer.GetRequestMilliseconds().ToString());
+            context.Response.AppendHeader("X-Serpentine-HandlerTime", requestTimer.GetHandlerMilliseconds().ToString());
 
             //Inject html
             if (context.Response.ContentType == MediaTypeNames.Text.Html)
             {
-                context.Response.Write($"RequestTime: {requestElapsed}ms<br/>HandlerTime: {handlerElapsed}ms");
+                context.Response.Write($"RequestTime: {requestTimer.GetRequestMilliseconds()}ms<br/>HandlerTime: {requestTimer.GetHandlerMilliseconds()}ms");
             }
         }
 
