42f7ce5a1bd4ed6ace14d5e820e5a642b4356be0
Morgan Creekmore <morgan@creekmore.email>
2019-02-03 01:22:17 -0600

    Added additional unittest for non html

--- tests/Serpentine.IISModule.Tests/MetricsEventHandlerFacts.cs
+++ tests/Serpentine.IISModule.Tests/MetricsEventHandlerFacts.cs
@@ -28,5 +28,27 @@ namespace Serpentine.IISModule.Tests
             httpContext.Response.Received().AppendHeader(Arg.Any<string>(), Arg.Any<string>());
             httpContext.Response.Received().Write(Arg.Any<string>());
         }
+
+        [Fact]
+        public void MetricsEventHandler_handler_doesnt_write_to_body_if_not_html()
+        {
+            var httpContext = Substitute.For<HttpContextBase>();
+            httpContext.Response.ContentType.Returns(MediaTypeNames.Text.Xml);
+            var storage = new ApplicationStorageMock();
+            var response = new MetricsResponse();
+            var context = new MetricTaskContext(storage, response);
+
+            var handler = new MetricsEventHandler(context);
+
+            //Act
+            handler.BeginRequest(httpContext);
+            handler.PreHandler(httpContext);
+            handler.PostHandler(httpContext);
+            handler.EndRequest(httpContext);
+
+            //Assert
+            httpContext.Response.Received().AppendHeader(Arg.Any<string>(), Arg.Any<string>());
+            httpContext.Response.DidNotReceive().Write(Arg.Any<string>());
+        }
     }
 }
